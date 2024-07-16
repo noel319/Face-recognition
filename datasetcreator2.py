90329e5c5fdaa9e9c7ab67617d514090628d586c
noel319 <azeisl917@gmail.com>
2024-07-17 00:42:53 +0300

    init commit

--- datasetcreator2.py
+++ datasetcreator2.py
@@ -1,14 +1,13 @@
-import cv2
-import cv2.cv as cv
+import cv2 as cv
 import sqlite3
 import numpy as np
-from Tkinter import *
+from tkinter import *
 
 root=Tk()
 
-detector=cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+detector=cv.CascadeClassifier('haarcascade_frontalface_default.xml')
 #detector=cv2.CascadeClassifier('lbpcascade_frontalface.xml')
-0cap=cv2.VideoCapture(0)
+cap=cv.VideoCapture(0)
 
 def insertOrUpdate(db_file):
     conn=sqlite3.connect(db_file)
@@ -26,29 +25,29 @@ def insertOrUpdate(db_file):
     conn.close()
     
     
-Id=raw_input('Enter the User ID')
-Name=raw_input('Enter the User Name')
+Id=input('Enter the User ID:')
+Name=input('Enter the User Name:')
 
-database="D:\IOT Project for Semester IV\Program\Face Recognition\Attendance.db"
+database= r"D:\work\AI\Face recognition\Attendance.db"
 insertOrUpdate(database)
-sampleNum=0;
+sampleNum=0
 while(True):
     ret, img = cap.read()
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    gray = cv.cvtColor(img, cv2.COLOR_BGR2GRAY)
     t = cv.GetTickCount()
     faces = detector.detectMultiScale(gray, 1.4, 5)
     t = cv.GetTickCount() - t
     for (x,y,w,h) in faces:
-        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+        cv.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
         sampleNum=sampleNum+1
-        cv2.imwrite("dataset2/User."+Id +'.'+ str(sampleNum) + ".jpg", gray[y:y+h,x:x+w])
+        cv.imwrite("dataset2/User."+Id +'.'+ str(sampleNum) + ".jpg", gray[y:y+h,x:x+w])
         
-        cv2.imshow('frame',img)
-    if(cv2.waitKey(100) & 0xFF == ord('q')):
+        cv.imshow('frame',img)
+    if(cv.waitKey(100) & 0xFF == ord('q')):
         break
     elif(sampleNum>70):
         break
-print "detection time = %gms" % (t/(cv.GetTickFrequency()*1000.))
+print("detection time = %gms" % (t/(cv.GetTickFrequency()*1000.)))
 print("Faces Found:",len(faces))
 cap.release()
-cv2.destroyAllWindows()    
+cv.destroyAllWindows()    
